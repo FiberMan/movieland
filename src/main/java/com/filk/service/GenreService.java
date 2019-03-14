d7ef17c177b860d54a80f2a854b81b34df107562
Yurii Filimonchuk <yfilimonchuk@luxoft.com>
2019-03-14 18:52:44 +0200

    [t-1] Create genre cache 1. As list of genres is rather static, we can save time on DB calling. Instead of reading genres from DB, we will create genre cache to store them all. 2. In order to have relevant information, genre cache should be updated according to DB data each 4 hours.

--- src/main/java/com/filk/service/GenreService.java
+++ src/main/java/com/filk/service/GenreService.java
@@ -1,9 +1,11 @@
 package com.filk.service;
 
+import com.filk.dao.GenreDao;
 import com.filk.entity.Genre;
 
 import java.util.List;
 
 public interface GenreService {
+    void setGenreDao(GenreDao genreDao);
     List<Genre> getAll();
 }
--- src/main/java/com/filk/service/impl/DefaultGenreService.java
+++ src/main/java/com/filk/service/impl/DefaultGenreService.java
@@ -4,21 +4,32 @@ import com.filk.dao.GenreDao;
 import com.filk.entity.Genre;
 import com.filk.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class DefaultGenreService implements GenreService {
     private GenreDao genreDao;
 
+    @Value("${genre.cacheUpdatePeriodHours}")
+    private long cacheUpdatePeriodHours;
+    private LocalDateTime lastUpdatedCache;
+    private List<Genre> genresCache;
+
     @Autowired
-    public DefaultGenreService(GenreDao genreDao) {
+    public void setGenreDao(GenreDao genreDao) {
         this.genreDao = genreDao;
     }
 
     @Override
     public List<Genre> getAll() {
-        return genreDao.getAll();
+        if(lastUpdatedCache == null || lastUpdatedCache.plusHours(cacheUpdatePeriodHours).isBefore(LocalDateTime.now())) {
+            genresCache = genreDao.getAll();
+            lastUpdatedCache = LocalDateTime.now();
+        }
+        return genresCache;
     }
 }
